58fc6dea628f936f67118596fe53ebafce591ab8
Aluno <aluno@naverecife.local>
2018-11-21 12:52:49 -0300

    Pathfinding_0.1

---
+++ Assets/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	[SerializeField] private float speed;
+	[SerializeField] private Transform target;
+
+	void Start() {
+		
+	}
+	
+	void FixedUpdate () {
+
+		print(Physics.Linecast(transform.position, target.position));
+
+		return;
+		if(Physics.Linecast(transform.position, target.position, 9)){
+			Debug.DrawLine(transform.position, target.position);
+			transform.position = Vector3.MoveTowards(transform.position, target.position, speed);
+		}
+		else{
+			print("Wall");
+		}
+
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField] private float speed;
+	[SerializeField] private Rigidbody2D rb;
+	
+	void FixedUpdate () {
+		float x = Input.GetAxisRaw("Horizontal");
+		float y = Input.GetAxisRaw("Vertical");
+
+		rb.velocity = new Vector2(x * speed, y * speed);
+	}
+}
